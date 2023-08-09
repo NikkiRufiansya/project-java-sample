3302b70dc47f3f6f435a8b216e4dd793801c3420
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2023-08-09 11:09:59 +0700

    callback finish

--- app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -2,20 +2,48 @@ package com.sourcey.materiallogindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.guardsquare.dexguard.rasp.callback.DetectionReport;
 
 public class MainActivity extends AppCompatActivity {
+    private static Context context;
+
+    private static boolean emulatorDetected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (emulatorDetected == true) {
+                    myToast();
+                    finishAffinity();
+                }
+            }
+        }, 800);
+
     }
 
-    public static void debugCallback(long debugInfo)
-    {
-        // This method will be called when an emulator is detected.
-        Log.i("HelloWorldActivity", "debug detected. DebugInfo : " + debugInfo);
+    public void myToast() {
+        Toast.makeText(context, "Kami mendeteksi adanya kejanggalan pada konfigurasi perangkatmu, " +
+                "Untuk Alasan keamanan aplikasi tidak dapat digunakan", Toast.LENGTH_SHORT).show();
     }
+
+
+    public static void detectionReport(DetectionReport detectionReport) {
+        if (detectionReport.isRunningInEmulator()) {
+            Log.d("hello", "emulator detected");
+            emulatorDetected = true;
+        }
+    }
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val agp_version by extra("7.4.1")
-    val agp_version1 by extra("8.0.1")
+    val agp_version by extra("8.0.1")
     repositories {
         maven {
             setUrl("/Users/rmltech/Documents/GuardSqurare/DexGuard-9.4.14/lib")
         }
+        flatDir {
+            dirs("libs")
+        }
         dependencies {
-
             classpath("com.guardsquare:dexguard-gradle-plugin:+")
         }
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.0.1" apply false
 }
+
 
