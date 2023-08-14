f6e09cb7627f3c7df3398e2f7c60313ece8fc11d
TechnicalRML <technical@rml.co.id>
2023-08-14 09:37:56 +0700

    Update 09.37

--- app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -3,19 +3,37 @@ package com.sourcey.materiallogindemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static Boolean emulatorDetection = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Handler handler = new Handler(Looper.getMainLooper());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (emulatorDetection==true){
+                    MyToast();
+                }
+            }
+        },600);
+    }
+    public void MyToast(){
+        Toast.makeText(this, "Aplikasi Berjalan Diemulator",Toast.LENGTH_SHORT).show();
+        finishAffinity();
     }
 
-    public static void debugCallback(long debugInfo)
+    public static void emulatorCallback(long debugInfo)
     {
         // This method will be called when an emulator is detected.
-        Log.i("HelloWorldActivity", "debug detected. DebugInfo : " + debugInfo);
+        Log.i("HelloWorldActivity", "Emulator detected. DebugInfo : " + debugInfo);
+        emulatorDetection=true;
     }
 }
